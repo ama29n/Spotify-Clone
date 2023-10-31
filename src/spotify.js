7e0937a496e5756911180824a0a525c55fdcc6fe
ama29n <ama3149n@gmail.com>
2023-11-01 04:58:47 +0530

    redirect uri

--- src/spotify.js
+++ src/spotify.js
@@ -23,7 +23,7 @@ const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 // Redirect URI 
-const redirectUri = "spotify-clone-ama29n.vercel.app/";
+const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 // const redirectUri = "http://localhost:3000/";
 
 const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}
